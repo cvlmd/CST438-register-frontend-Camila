6264ce294c3f6ebb818b978a7f855b2742dcf3d0
cvlmd <camila.veille@esme.fr>
2023-10-25 06:41:53 +0200

    assignment6

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import './App.css';
 import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import ListAssignment from './components/ListAssignment';
 import GradeAssignment from './components/GradeAssignment';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <h2>Gradebook</h2>
       <BrowserRouter>
+      
           <div>
-            <Switch>
-              <Route exact path="/" component={ListAssignment} />
-              <Route path="/gradeAssignment" component={GradeAssignment} />
-              <Route render={ () => <h1>Page not found</h1>} />
+          <nav>
+            <Link to="/">Assignment</Link> |
+            <Link to="/admin">Admin</Link> |
+            <Link to="/login">Login</Link>  
+          </nav>
+          [06:15] Coralie Rouveyrol
+
+<Switch>
+
+            <Route exact path="/" component={ListAssignment} />
+
+            <Route path="/schedule" component={GradeAssignment} />
+
+            <Route path="/admin" component={AddAssignment} />
+
+            <Route path="/login" component={Login} />
+
+            <Route render={() => <h1>Page not found</h1>} />
+
             </Switch>
           </div>
         </BrowserRouter>
